f4c32d2b8441a4f60dae0e7fe418435ab49ebdd2
Badal Badshah <89441131+badalbadshah@users.noreply.github.com>
2024-03-07 13:31:19 +0530

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id   = "<azure_subscription_id>"
+  tenant_id         = "<azure_subscription_tenant_id>"
+  client_id         = "<service_principal_appid>"
+  client_secret     = "<service_principal_password>"
+}
